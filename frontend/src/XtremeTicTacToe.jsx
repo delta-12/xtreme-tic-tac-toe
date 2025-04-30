5c1efadacd8d3012237df38c1dd9430f4f093acb
delta-12 <btesq235@gmail.com>
2025-04-30 02:05:47 -0400

    send player status

--- frontend/src/XtremeTicTacToe.jsx
+++ frontend/src/XtremeTicTacToe.jsx
@@ -9,7 +9,10 @@ export default function ExtremeTicTacToe() {
     const [activeBoard, setActiveBoard] = useState(null); // null = any board
     const [smallWins, setSmallWins] = useState(Array(9).fill(null)); // track mini board winners
     const [player, setPlayer] = useState(null);
-    
+    const [gameID, setGameID] = useState(null);
+    const [playerXStatus, setPlayerXStatus] = useState("Disconnected");
+    const [playerOStatus, setPlayerOStatus] = useState("Disconnected");
+
     const socket = useRef(null);
 
     useEffect(() => {
@@ -36,8 +39,15 @@ export default function ExtremeTicTacToe() {
                         setActiveBoard(parsed_message.state.active_board);
                         setSmallWins(parsed_message.state.small_wins);
                         break;
-                    case "player":
+                    case "game_id":
+                        setGameID(parsed_message.game_id)
+                        break;
+                    case "player_assign":
                         setPlayer(parsed_message.player)
+                        break;
+                    case "player_status":
+                        setPlayerXStatus(parsed_message.player_x);
+                        setPlayerOStatus(parsed_message.player_o);
                         break;
                     default:
                         break;
@@ -116,11 +126,32 @@ export default function ExtremeTicTacToe() {
                     {overallWinner} wins the game!
                 </div>
             ) : (
-                <>
-                    <div className="text-lg">Player: {player}</div>
-                    <div className="text-lg">Current Player: {currentPlayer}</div>
-                </>
+                <div className="text-lg">Game ID: {gameID}</div>
             )}
+            <div className="flex w-full lg:max-w-4xl lg:w-2/5 gap-5">
+                <div className="w-full rounded-lg border-2 p-1 sm:p-2">
+                    <div className="grid grid-cols-2 gap-3">
+                        <div>
+                            <h2 className="text-xl font-bold">You</h2>
+                            {player ? player === "X" ? playerXStatus : playerOStatus : ""}
+                        </div>
+                        <div className={`w-full h-full flex items-center justify-center text-6xl sm:text-8xl font-extrabold ${player ? player === "X" ? "text-green-600" : "text-red-600" : ""}`}>
+                            {player}
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full rounded-lg border-2 p-1 sm:p-2">
+                    <div className="grid grid-cols-2 gap-3">
+                        <div>
+                            <h2 className="text-xl font-bold">Opponent</h2>
+                            {player ? player === "X" ? playerOStatus : playerXStatus : ""}
+                        </div>
+                        <div className={`w-full h-full flex items-center justify-center text-6xl sm:text-8xl font-extrabold ${player ? player === "X" ? "text-red-600" : "text-green-600" : ""}`}>
+                            {player ? player === "X" ? "O" : "X" : ""}
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="grid grid-cols-3 gap-3 sm:gap-4 max-w-4xl lg:w-2/5 sm:w-full p-2 sm:p-4">
                 {board.map((smallBoard, bigIndex) => (
                     <div
@@ -128,10 +159,11 @@ export default function ExtremeTicTacToe() {
                         className={`gap-1 p-2 sm:gap-2 rounded-lg border-2 p-1 sm:p-2 aspect-square transition ${activeBoard === null || activeBoard === bigIndex
                             ? "border-blue-500"
                             : "border-gray-400 opacity-50"
-                            } ${smallWins[bigIndex] ? "flex items-center bg-green-100" : "grid grid-cols-3"}`}
+                            } ${smallWins[bigIndex] ? "flex items-center" : "grid grid-cols-3"}
+                            ${smallWins[bigIndex] ? smallWins[bigIndex] === "X" ? "bg-green-100" : "bg-red-100" : ""}`}
                     >
                         {smallWins[bigIndex] ? (
-                            <div className="w-full h-full flex items-center justify-center text-6xl sm:text-8xl font-extrabold text-green-600 animate-pop">
+                            <div className={`w-full h-full flex items-center justify-center text-6xl sm:text-8xl font-extrabold animate-pop ${smallWins[bigIndex] ? smallWins[bigIndex] === "X" ? "text-green-600" : "text-red-600" : ""}`}>
                                 {smallWins[bigIndex]}
                             </div>
                         ) : (
@@ -141,7 +173,7 @@ export default function ExtremeTicTacToe() {
                                     className="w-full h-full flex items-center justify-center border rounded-md text-base sm:text-xl font-bold hover:bg-gray-200 transition active:scale-95"
                                     onClick={() => handleClick(bigIndex, smallIndex)}
                                 >
-                                    <span className="block w-6 h-6 sm:w-8 sm:h-8 items-center">
+                                    <span className={`block w-6 h-6 sm:w-8 sm:h-8 items-center ${cell ? cell === "X" ? "text-green-600" : "text-red-600" : ""}`}>
                                         {cell}
                                     </span>
                                 </button>
---
+++ game_server/server.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+
+import os
+import signal
+import asyncio
+import json
+import secrets
+import websockets
+from enum import Enum, auto
+from random import randint
+from uuid import uuid4
+
+
+class Error(Enum):
+    GAME_NOT_FOUND = auto()
+    INVALID_TYPE = auto()
+    INVALID_GAME_ID = auto()
+    INVALID_SAVED_GAME = auto()
+    INVALID_MOVE = auto()
+
+
+JOIN = {}
+GAMES = {}
+
+
+# Send an error message
+async def error(websocket, error):
+    error_message = ""
+
+    if error == Error.GAME_NOT_FOUND:
+        error_message = "Game not found"
+    elif error == Error.INVALID_TYPE:
+        error_message = "Invalid message type"
+    elif error == Error.INVALID_GAME_ID:
+        error_message = "Invalid game ID"
+    elif error == Error.INVALID_SAVED_GAME:
+        error_message = "Invalid saved game"
+    elif error == Error.INVALID_MOVE:
+        error_message = "Invalid move"
+
+    event = {"type": "error", "error": error_message}
+
+    await websocket.send(json.dumps(event))
+
+
+# Send game state to player
+async def send_game_state(key):
+    await JOIN[key]["websocket"].send(
+        json.dumps({"type": "state", "state": GAMES[JOIN[key]["game_id"]]["state"]})
+    )
+
+
+# Send player info
+async def send_player_info(key):
+    game_id = JOIN[key]["game_id"]
+    player = "X"
+
+    if GAMES[game_id]["player_o"] == key:
+        player = "O"
+
+    await JOIN[key]["websocket"].send(
+        json.dumps({"type": "game_id", "game_id": game_id})
+    )
+    await JOIN[key]["websocket"].send(
+        json.dumps({"type": "player_assign", "player": player})
+    )
+
+
+async def send_player_status(key):
+    game_id = JOIN[key]["game_id"]
+    player_status = {
+        "type": "player_status",
+        "player_x": (
+            "Connected" if GAMES[game_id]["player_x"] is not None else "Disconnected"
+        ),
+        "player_o": (
+            "Connected" if GAMES[game_id]["player_o"] is not None else "Disconnected"
+        ),
+    }
+
+    if GAMES[game_id]["player_x"] is not None:
+        await JOIN[GAMES[game_id]["player_x"]]["websocket"].send(
+            json.dumps(player_status)
+        )
+    if GAMES[game_id]["player_o"] is not None:
+        await JOIN[GAMES[game_id]["player_o"]]["websocket"].send(
+            json.dumps(player_status)
+        )
+
+
+# Handle connection
+async def handle_connection(key):
+    async for message in JOIN[key]["websocket"]:
+        print(json.loads(message))
+        # TODO handle game end
+
+
+async def remove_connecion(key):
+    game_id = JOIN[key]["game_id"]
+
+    if GAMES[game_id]["player_o"] == key:
+        GAMES[game_id]["player_o"] = None
+    else:
+        GAMES[game_id]["player_x"] = None
+
+    try:
+        if GAMES[game_id]["player_x"] is None and GAMES[game_id]["player_o"] is None:
+            del GAMES[game_id]
+            print("Ended game", game_id)
+        else:
+            await send_player_status(key)
+    except:
+        pass
+
+    del JOIN[key]
+    print("Closed connection", key)
+
+
+# Start handling connection from player, cleanup on close
+async def start_connection(key):
+    try:
+        await send_player_info(key)
+        await send_player_status(key)
+        await send_game_state(key)
+        await handle_connection(key)
+    finally:
+        await remove_connecion(key)
+
+
+def add_connection(websocket):
+    key = secrets.token_urlsafe(12)
+    JOIN[key] = {
+        "websocket": websocket,
+        "game_id": None,
+    }
+
+    print("New connection", key)
+
+    return key
+
+
+async def handle_game_id(websocket, game_id):
+    if game_id not in GAMES:
+        await error(websocket, Error.GAME_NOT_FOUND)
+    elif (
+        GAMES[game_id]["player_x"] is not None
+        and GAMES[game_id]["player_o"] is not None
+    ):
+        await error(websocket, Error.INVALID_GAME_ID)
+    else:
+        key = add_connection(websocket)
+        player = "player_x"
+
+        if GAMES[game_id]["player_o"] is None:
+            player = "player_o"
+
+        JOIN[key]["game_id"] = game_id
+        GAMES[game_id][player] = key
+        await start_connection(key)
+
+
+async def handle_saved_game(websocket, saved_game):
+    # TODO
+    if not saved_game:
+        await error(websocket, Error.INVALID_SAVED_GAME)
+    else:
+        key = add_connection(websocket)
+        # TODO
+        await start_connection(key)
+
+
+async def handle_new_game(websocket):
+    key = add_connection(websocket)
+    game_id = str(uuid4())
+    GAMES[game_id] = {
+        "player_x": None,
+        "player_o": None,
+        "state": {
+            "board": [],
+            "current_player": "X",
+            "active_board": None,
+            "small_wins": [],
+        },
+    }
+    print("Started game", game_id)
+    for i in range(9):
+        GAMES[game_id]["state"]["board"].append([])
+        GAMES[game_id]["state"]["small_wins"].append(None)
+        for _ in range(9):
+            GAMES[game_id]["state"]["board"][i].append(None)
+    if randint(0, 1) == 0:
+        GAMES[game_id]["player_x"] = key
+    else:
+        GAMES[game_id]["player_o"] = key
+    JOIN[key]["game_id"] = game_id
+    await start_connection(key)
+
+
+# Handle a connection initial connection
+async def handler(websocket):
+    message = await websocket.recv()
+    parsed_message = json.loads(message)
+
+    if "type" not in parsed_message or parsed_message["type"] != "connect":
+        await error(websocket, Error.INVALID_TYPE)
+    elif "game_id" in parsed_message and parsed_message["game_id"] is not None:
+        await handle_game_id(websocket, parsed_message["game_id"])
+    elif "saved_game" in parsed_message and parsed_message["saved_game"] is not None:
+        await handle_saved_game(websocket, parsed_message["saved_game"])
+    else:
+        await handle_new_game(websocket)
+
+
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("GAME_SERVER_PORT", "10801"))
+    async with websockets.serve(handler, "", port):
+        await stop
+
+
+if __name__ == "__main__":
+    print("Server started!")
+    asyncio.run(main())
+    print("Server stopped!")
